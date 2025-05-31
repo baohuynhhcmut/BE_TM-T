024539c4fa88581e3830ccafad4d4f76c0cf45af
Bao Huynh Gia <bao.huynh2201@hcmut.edu.vn>
2025-05-31 17:35:30 +0700

    merge code

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const routes = require('./router'); 
-const db = require('./config/database'); 
-const seedData = require('./DataScript/dataseed')
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const routes = require("./router");
+const db = require("./config/database");
+const seedData = require("./DataScript/dataseed");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-app.use(cors({
-    origin: "http://localhost:3000", 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
-
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 routes(app);
 
 async function startServer() {
-    try {
-      
-     await db.sync({ force: true});
-    await seedData();
-  
-      app.listen(port, () => {
-        console.log(`Server is running at http://localhost:${port}`);
-      });
-    } catch (error) {
-      console.error('Error starting server:', error);
-    }
+  try {
+    await db.sync({ force: true });
+
+    // await seedData();
+
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
   }
-  
-  startServer();
+}
+
+startServer();
